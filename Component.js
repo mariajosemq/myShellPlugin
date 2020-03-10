7137eaaa8f6901cf0b3268f7899606cffb43522c
mariajosemq <mariajosemartinezquintero@gmail.com>
2020-03-10 22:18:51 +0000

    first commit

---
+++ Component.js
@@ -0,0 +1,73 @@
+sap.ui.define([
+	"sap/ui/core/Component",
+	"sap/m/Button",
+	"sap/m/Bar",
+	"sap/m/MessageToast"
+], function (Component, Button, Bar, MessageToast) {
+
+	return Component.extend("com.sap.myshellplugin.Component", {
+
+		metadata: {
+			"manifest": "json"
+		},
+
+		init: function () {
+			var rendererPromise = this._getRenderer();
+
+			// This is example code. Please replace with your implementation!
+			this.renderRecastChatbot():
+
+		},
+		renderRecastChatbot: function() {
+			if (!document.getElementById("recast-webchat")) {
+				var s = document.createElement("script");
+				s.setAttribute("id", "recast-webchat");
+				s.setAttribute("src", "https://cdn.cai.tools.sap/webchat/webchat.js");
+				document.body.appendChild(s);
+			}
+			s.setAttribute("channelId", "f4b2874f-161b-48b0-aa92-abbd76a3464d");
+			s.setAttribute("token", "a5692498e93cd1fdb13763e4f147a1b2");
+		},
+
+		/**
+		 * Returns the shell renderer instance in a reliable way,
+		 * i.e. independent from the initialization time of the plug-in.
+		 * This means that the current renderer is returned immediately, if it
+		 * is already created (plug-in is loaded after renderer creation) or it
+		 * listens to the &quot;rendererCreated&quot; event (plug-in is loaded
+		 * before the renderer is created).
+		 *
+		 *  @returns {object}
+		 *      a jQuery promise, resolved with the renderer instance, or
+		 *      rejected with an error message.
+		 */
+		_getRenderer: function () {
+			var that = this,
+				oDeferred = new jQuery.Deferred(),
+				oRenderer;
+
+			that._oShellContainer = jQuery.sap.getObject("sap.ushell.Container");
+			if (!that._oShellContainer) {
+				oDeferred.reject(
+					"Illegal state: shell container not available; this component must be executed in a unified shell runtime context.");
+			} else {
+				oRenderer = that._oShellContainer.getRenderer();
+				if (oRenderer) {
+					oDeferred.resolve(oRenderer);
+				} else {
+					// renderer not initialized yet, listen to rendererCreated event
+					that._onRendererCreated = function (oEvent) {
+						oRenderer = oEvent.getParameter("renderer");
+						if (oRenderer) {
+							oDeferred.resolve(oRenderer);
+						} else {
+							oDeferred.reject("Illegal state: shell renderer not available after recieving 'rendererLoaded' event.");
+						}
+					};
+					that._oShellContainer.attachRendererCreatedEvent(that._onRendererCreated);
+				}
+			}
+			return oDeferred.promise();
+		}
+	});
+});
